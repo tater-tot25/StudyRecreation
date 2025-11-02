97ff23d173afc44241180509eacff451e710d31f
Leena Butt <u1283933@gcloud.utah.edu>
2025-11-02 16:02:58 -0700

    Changed end of study text

--- html/pages/default/end.php
+++ html/pages/default/end.php
@@ -2,13 +2,13 @@
     <div id="thankyou_div_1">
         <h2>Feedback</h2>
         <p>
-            Do you have any comment about the study, for example concerning the clarity of the instructions or technical issues you might have experienced? (optional)
+            Do you have any comments about the study, for example concerning the clarity of the instructions or technical issues you might have experienced? (optional)
         </p>
         <div>
             <textarea class="form-control" id="general_comments" rows="4" cols="80"
-                      placeholder="Feel free to leave it blank, if you don't have any comment :)"
+                      placeholder="Feel free to leave it blank if you don't have any comments."
                       style="width: 1100px;"></textarea><br>
-            <input type="button" value="Submit and Get Paid" id="thankyou_btn" class="btn btn-success">
+            <input type="button" value="Submit" id="thankyou_btn" class="btn btn-success">
         </div>
     </div>
 
@@ -20,55 +20,11 @@
         <div id="completion_code_container" style="margin-bottom: 20px; display: none;">
             <p><strong>Your completion code:</strong></p>
             <h3 id="completion_code" style="font-family: monospace; font-size: 1.5em;"></h3>
-            <p>Please copy and paste this code into Prolific.</p>
+            <p>Please copy and paste this code for proof of completion.</p>
         </div>
 
-        <p>
-            Click
-            <a id="prolific_link"
-               href="https://app.prolific.co/submissions/complete?cc=CDR2RKQG"
-               target="_BLANK"><strong>here</strong></a>
-            to report your task completion to the Prolific system.
-        </p>
+        <p>You can now close the tab.</p>
 
-        <p>Now you can close the tab. If you have any questions or general comments, do not hesitate to contact us:</p>
-
-        <div align="justify" style="max-width: 100%; display: flex; margin-left: auto; margin-right: auto;">
-            <div align="justify" style="max-width: 30%; display: block; margin-left: 0; margin-right: auto;">
-                <p><strong>Tingying He</strong></p>
-                <p>PhD Student</p>
-                <p><a href="https://www.aviz.fr/Main/HomePage" target="_blank">AVIZ</a> Research Team</p>
-                <p>Université Paris-Saclay</p>
-                <p>
-                    <a href="mailto:tingying.he@inria.fr?cc=petra.isenberg@inria.fr,tobias.isenberg@inria.fr&amp;subject=%5BTexture%20Design%5D%20Question%20about%20the%20experiment1">
-                        tingying.he@inria.fr
-                    </a>
-                </p>
-            </div>
-
-            <div align="justify" style="max-width: 30%; display: block; margin-left: 0; margin-right: auto;">
-                <p><strong>Petra Isenberg</strong></p>
-                <p>Senior Research Scientist</p>
-                <p><a href="https://www.aviz.fr/Main/HomePage" target="_blank">AVIZ</a> Research Team</p>
-                <p>Inria Saclay Île-de-France</p>
-                <p>
-                    <a href="mailto:petra.isenberg@inria.fr?cc=tingying.he@inria.fr,tobias.isenberg@inria.fr&amp;subject=%5BTexture%20Design%5D%20Question%20about%20the%20experiment1">
-                        petra.isenberg@inria.fr
-                    </a>
-                </p>
-            </div>
-
-            <div align="justify" style="max-width: 30%; display: block; margin-left: 0; margin-right: auto;">
-                <p><strong><a href="https://tobias.isenberg.cc/" target="_blank">Tobias Isenberg</a></strong></p>
-                <p>Senior Research Scientist</p>
-                <p><a href="https://www.aviz.fr/Main/HomePage" target="_blank">AVIZ</a> Research Team</p>
-                <p>Inria Saclay Île-de-France</p>
-                <p>
-                    <a href="mailto:tobias.isenberg@inria.fr?cc=tingying.he@inria.fr,petra.isenberg@inria.fr&amp;subject=%5BTexture%20Design%5D%20Question%20about%20the%20experiment1">
-                        tobias.isenberg@inria.fr
-                    </a>
-                </p>
-            </div>
         </div>
     </div>
 </div>
